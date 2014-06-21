fcbf2cf6f8cabc6161141a38d2dbf0b5f4cdb897
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2014-06-21 13:22:45 +0600

    removed some files

--- tic-tac-toe/javascripts/script.js
+++ tic-tac-toe/javascripts/script.js
@@ -51,3 +51,58 @@ function resetGame(){
 
 
 playbutton.addEventListener("click", resetGame);
+
+//todo-implement ai 
+
+//todo create an ai for tic-tac-toe
+
+//check wheather two in a row or not
+function twoinarow(player){
+}
+
+//Win:if you have two in a row, you can place a third to get three in a row.
+function win(player){
+
+}
+
+//Block:If the opponent has two in a row, you must play the third to block the opponent.
+function block(player){
+
+}
+
+//Fork:Create an opportunity where you have two threats to win (two non-blocked lines of 2).
+function fork(player){
+
+}
+
+//Blocking an opponent's fork:If there is a configuration where the opponent can fork, you must block that fork.
+function blockfork(player){
+
+}
+
+//Center:You play the center if open.
+function center(player){
+
+}
+
+//Opposite corner:If the opponent is in the corner, you play the opposite corner.
+function oppositecorner(player){
+
+}
+//Empty corner:You play in a corner square.
+function emptycorner(player){
+
+
+}
+
+//Empty side:You play in a middle square on any of the 4 sides.
+function emptyside(player){
+
+
+}
+
+function ai(player){
+
+
+
+}

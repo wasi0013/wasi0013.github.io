d9b6579e05c5441863d3c8e1b23a170f768ddb57
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2014-06-26 22:05:11 +0600

    Fix bug

--- js/signin.js
+++ js/signin.js
@@ -3,31 +3,33 @@
        po.src = 'https://apis.google.com/js/client:plusone.js';
        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
      })();
-function signinCallback(authResult) {
-  if (authResult['status']['signed_in']) {
-    alert("Sign in Successful");
 
-       $.ajax({
+var signin=false;
+function signinCallback(authResult) {
+
+  if(!signin){if (authResult['status']['signed_in']) {
+    signin=true;
+
+    $.ajax({
         url: "https://www.googleapis.com/plus/v1/people/me?access_token="+authResult["access_token"],
         type: 'GET',
         dataType: 'json',
         
         success: function(data) {
-          //do something with the data
+          alert("Welcome! "+data["displayName"]+" you are logged in!")
         }, 
 
     });
-
     // Update the app to reflect a signed in user
     // Hide the sign-in button now that the user is authorized, for example:
     document.getElementById('signinButton').setAttribute('style', 'display: none');
   } else {
-    alert("Sign in failed");
+    
     // Update the app to reflect a signed out user
     // Possible error values:
     //   "user_signed_out" - User is signed-out
     //   "access_denied" - User denied access to your app
     //   "immediate_failed" - Could not automatically log in the user
     console.log('Sign-in state: ' + authResult['error']);
-  }
+  }}
 }

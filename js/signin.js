31f1ad233e81a3eeb64e419192cd8a6886397825
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2014-06-26 20:39:56 +0600

    ...

--- js/signin.js
+++ js/signin.js
@@ -10,7 +10,7 @@ function signinCallback(authResult) {
     $.ajax({
         url: "https://www.googleapis.com/plus/v1/people/",
         data: {
-          userId:"me",
+          userId="me",
         },
 
         type: 'GET',

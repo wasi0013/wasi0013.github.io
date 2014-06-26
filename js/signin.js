9bfe994b01165ad5b0847da679c5ab4dfdf893e1
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2014-06-26 20:42:11 +0600

    ...

--- js/signin.js
+++ js/signin.js
@@ -5,19 +5,16 @@
      })();
 function signinCallback(authResult) {
   if (authResult['status']['signed_in']) {
-    alert("Sign in Successful");
+    alert("Sign in Successful"+authResult);
 
     $.ajax({
-        url: "https://www.googleapis.com/plus/v1/people/",
-        data: {
-          userId="me",
-        },
-
+        url: "https://www.googleapis.com/plus/v1/people/me",
         type: 'GET',
         dataType: 'json',
         success: function(data) {
             alert(data);
-        }
+        }, 
+
     });
     // Update the app to reflect a signed in user
     // Hide the sign-in button now that the user is authorized, for example:
